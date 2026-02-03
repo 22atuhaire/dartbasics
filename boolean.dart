4952190cce8a923fd71fb7bca571dd5478979d75
22atuhaire <atuhaire.abiudi@student.utamu.ac.ug>
2026-02-03 11:56:27 +0300

    Add boolean variable declaration and string comparison example in Dart

---
+++ boolean.dart
@@ -0,0 +1,13 @@
+void main() {
+  // declearing a boolean variable with a true value
+  bool val1 = true;
+
+  //decrearing 2 string variables and comparing them
+  String str1 = "hello";
+  String str2 = "world";
+
+  bool val2 = (str1 == str2);
+
+  print(val1);
+  print(val2);
+}
