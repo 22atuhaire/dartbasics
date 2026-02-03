db2edde474a787ef79f4a72c8dbedeba3d72e3d3
22atuhaire <atuhaire.abiudi@student.utamu.ac.ug>
2026-02-03 14:23:27 +0300

    Add initial implementation of set declaration and usage in Dart

---
+++ sets.dart
@@ -0,0 +1,13 @@
+void main() {
+  //declairing a set of strings
+  Set<String> colors = {'Red', 'Green', 'Blue'};
+
+  //using a set constructor to create an empty set
+  Set<int> numbers = Set();
+  numbers.add(1);
+  numbers.add(2);
+  numbers.add(3);
+
+  print('Colors Set: $colors');
+  print('Numbers Set: $numbers');
+}
