a1390b656123fd52231305466bfe2fb8c82407ed
22atuhaire <atuhaire.abiudi@student.utamu.ac.ug>
2026-02-03 10:43:50 +0300

    Add initial implementation of variable declaration and arithmetic in Dart

---
+++ datatypes.dart
@@ -0,0 +1,16 @@
+
+void main(){
+    //Declaring a variable of type int
+    int num1 = 10;
+
+   //declaring a variable of type double
+    double num2 = 20.5;
+
+    print('$num1');
+    print('$num2');
+
+    //adding int and double resulting in double
+    var sum = num1 + num2;
+    print('$sum');
+
+}
