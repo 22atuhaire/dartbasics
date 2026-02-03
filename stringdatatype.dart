a6f2e03ababc4386798743d1f29aa5dfd033c0b8
22atuhaire <atuhaire.abiudi@student.utamu.ac.ug>
2026-02-03 11:36:44 +0300

    Add string datatype declaration and concatenation example in Dart

---
+++ stringdatatype.dart
@@ -0,0 +1,12 @@
+void main() {
+  // derclare a string datatype
+  String string_name = "my string is this";
+
+  //2 strings concatenation
+  String string1 = "coding is ";
+  String string2 = "fun";
+
+  print(string_name);
+
+  print(string1 + string2);
+}
